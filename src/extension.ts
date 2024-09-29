120e25678e627e22ac3b2d9fbf5eaa67a4d6d631
giosali <70548443+giosali@users.noreply.github.com>
2024-09-28 20:12:29 -0500

    Provide mechanism for enabling and disabling overview swipe

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,24 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class DisableTouchpadGesturesExtension extends Extension {
-  enable() {}
+  enable() {
+    const stage = global.stage as any;
+    stage.disconnectObject(Main.overview._swipeTracker._touchpadGesture);
+    stage.connectObject(
+      "captured-event::touchpad",
+      (_: Clutter.Actor, __: Clutter.Event) => {},
+      Main.overview._swipeTracker._touchpadGesture
+    );
+  }
 
-  disable() {}
+  disable() {
+    const gesture = Main.overview._swipeTracker._touchpadGesture;
+    const stage = global.stage as any;
+    stage.disconnectObject(gesture);
+    stage.connectObject(
+      "captured-event::touchpad",
+      gesture._handleEvent.bind(gesture),
+      gesture
+    );
+  }
 }
