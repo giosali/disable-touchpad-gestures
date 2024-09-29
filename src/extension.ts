c2be282164228b94984e92d8fe8946d191751ea8
giosali <70548443+giosali@users.noreply.github.com>
2024-09-28 19:55:40 -0500

    Remove unused statements

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,11 @@
-import GLib from "gi://GLib";
-import Gio from "gi://Gio";
-import Meta from "gi://Meta";
-import Shell from "gi://Shell";
+import Clutter from "gi://Clutter";
 
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class DisableTouchpadGesturesExtension extends Extension {
-  // TODO: handle later.
-  gsettings?: Gio.Settings;
-  animationsEnabled: boolean = true;
+  enable() {}
 
-  enable() {
-    // TODO: handle later.
-    this.gsettings = this.getSettings();
-    this.animationsEnabled =
-      this.gsettings!.get_value("padding-inner").deepUnpack() ?? 8;
-  }
-
-  disable() {
-    // TODO: handle later.
-    this.gsettings = undefined;
-  }
+  disable() {}
 }
