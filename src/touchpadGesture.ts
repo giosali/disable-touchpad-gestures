36cc2f0be82bdf1786b2693450bce4e9a425d4c0
giosali <70548443+giosali@users.noreply.github.com>
2024-10-12 15:29:00 -0500

    Create TouchpadGesture class

---
+++ src/touchpadGesture.ts
@@ -0,0 +1,51 @@
+import Clutter from "gi://Clutter";
+import GLib from "gi://GLib";
+import Gio from "gi://Gio";
+
+export default class TouchpadGesture {
+  private readonly event: string = "captured-event::touchpad";
+  private gesture: any;
+
+  constructor(gesture: any) {
+    this.gesture = gesture;
+  }
+
+  /**
+   * Disables the actual, underlying touchpad gesture.
+   */
+  public disable(): void {
+    const stage: any = this.getStage();
+    stage.disconnectObject(this.gesture);
+    stage.connectObject(
+      this.event,
+      (_: Clutter.Actor, __: Clutter.Event) => {},
+      this.gesture
+    );
+  }
+
+  /**
+   * Reenables the actual, underlying touchpad gesture.
+   */
+  public enable(): void {
+    const stage: any = this.getStage();
+    stage.disconnectObject(this.gesture);
+    stage.connectObject(
+      this.event,
+      this.gesture._handleEvent.bind(this.gesture),
+      this.gesture
+    );
+  }
+
+  public onSettingChanged(settings: Gio.Settings, key: string): void {
+    const variant: GLib.Variant<any> = settings.get_value(key);
+    if (variant.get_type_string() === "b") {
+      const value: boolean = variant.get_boolean();
+      value ? this.disable() : this.enable();
+    }
+  }
+
+  private getStage(): any {
+    const stage = global.stage as any;
+    return stage;
+  }
+}
